69f7fe98b3d31d952f11f4f5e6313458dd1b1019
saran <saran.sankaran@gmail.com>
2021-09-21 23:40:15 +0530

    Remove the unwanted app module

--- app/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("kotlin")
-}
-
-dependencies {
-    implementation("dev.sankaran.saran:samplelibrary:0.0.1")
-}
--- app/src/main/java/dev/sankaran/saran/app/App.kt
+++
@@ -1,7 +0,0 @@
-package dev.sankaran.saran.app
-
-import dev.sankaran.saran.samplelibrary.WORLD
-
-fun main() {
-    println("Hello $WORLD!!")
-}
